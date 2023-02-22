180b08434600abd24e860ed21bd7e99e4fd9714c
peakaw <>
2023-02-22 04:53:21 -0800

    add comment

--- src/single/CollateralAuctionHouse.sol
+++ src/single/CollateralAuctionHouse.sol
@@ -334,6 +334,27 @@ contract EnglishCollateralAuctionHouse {
     }
 }
 
+/// FixedDiscountCollateralAuctionHouse.sol
+
+// Copyright (C) 2018 Rain <rainbreak@riseup.net>, 2020 Reflexer Labs, INC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/*
+   This thing lets you sell some collateral at a fixed discount in order to instantly recapitalize the system
+*/
+
 contract FixedDiscountCollateralAuctionHouse {
     // --- Auth ---
     mapping (address => uint256) public authorizedAccounts;
